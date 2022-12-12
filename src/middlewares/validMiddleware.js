dd48b21c7db4362ae9f0ab16f47e8d168633e8d9
tensiJY <32132377+tensiJY@users.noreply.github.com>
2022-12-12 12:22:34 +0900

    Update validMiddleware.js

--- src/middlewares/validMiddleware.js
+++ src/middlewares/validMiddleware.js
@@ -1,7 +1,7 @@
 const { validationResult } = require(`express-validator`);
 const { getMessage } = require(`../modules/message.js`);
 const logger = require(`../modules/logger`);
-const ApiError = require(`../modules/ApiError`);
+
 /*
 errors: [
     {
@@ -24,28 +24,14 @@ exports.validError = (req, res, next) => {
   if (!error.isEmpty()) {
     logger.info(`validError`);
     logger.info(JSON.stringify(error));
-    //const path = req.originalUrl;
-    const errors = error.array();
-    const type = getMessage(errors[0].msg);
-    //logger.info(
-    // `validError > path : ${path}, code : ${messageObj.code}, message : ${messageObj.message}`
-    //);
-    throw new ApiError(type);
-    //return res.status(messageObj.status).json({
-    //  ...messageObj,
-    //   path: path,
-    // });
-  }
-  next();
-};
 
-/*
-exports.validErrors = (req, res, next) => {
-  const errors = validationResult(req);
-  console.log(errors);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    const errors = error.array();
+    //const type = getMessage(errors[0].msg);
+    const type = errors[0].msg;
+    console.log(type);
+    const err = new Error(`input valid error`);
+    err.type = type;
+    throw err;
   }
   next();
 };
-*/
