f3c8df10338bfd92b78a83ae7808fdb247d37301
tensiJY <32132377+tensiJY@users.noreply.github.com>
2022-12-12 13:05:00 +0900

    Update errorMiddleware.js

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,11 +1,17 @@
 const { getMessage } = require(`../modules/message`);
 const logger = require(`../modules/logger`);
 
+/**
+ * 404 page not found
+ * @param {*} req
+ * @param {*} res
+ * @returns
+ */
 const errorPageNotFound = (req, res) => {
   const errObj = getMessage(`PAGE_NOT_FOUND`);
   const path = req.path;
 
-  logger.error(`pageNotFound : ${path}`);
+  logger.error(`error page not found : ${path}`);
 
   return res.status(errObj.status).json({
     ...errObj,
@@ -13,15 +19,26 @@ const errorPageNotFound = (req, res) => {
   });
 };
 
+/**
+ * errorHandler : 공통 에러 처리를 담당
+ * @param {*} err
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
 const errorHandler = (err, req, res, next) => {
   const type = err.type || `INTERNAL_SERVER_ERROR`;
   const errObj = getMessage(type);
   const path = req.path;
-  logger.error(`errorHandler : ${path}`);
+  logger.error(`error > method : ${req.method}, path : ${path}`);
+  logger.error(err);
   logger.error(errObj);
 
   res.status(errObj.status).json({
-    ...errObj,
+    status: errObj.status,
+    result: errObj.result,
+    code: errObj.code,
+    message: errObj.message,
     path: path,
   });
 };
