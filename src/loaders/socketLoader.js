584b3de5d19258fa11669ab35053093d10c106ef
tensiJY <32132377+tensiJY@users.noreply.github.com>
2022-12-16 14:53:57 +0900

    Update socketLoader.js

--- src/loaders/socketLoader.js
+++ src/loaders/socketLoader.js
@@ -1,28 +1,8 @@
-const server = require(`./serverLoader`);
-const logger = require(`../modules/logger`);
+const { drvSocket, mgrSocket } = require(`../modules/socket`);
 
-const { Server } = require(`socket.io`);
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-  },
-});
+const socketLoader = (io) => {
+  drvSocket(io);
+  mgrSocket(io);
+};
 
-let counter = 0;
-const mIo = io.of(`/m`);
-mIo.use((socket, next) => {
-  const header = socket.handshake.headers[`authorization`];
-  logger.info(header);
-  next();
-});
-
-mIo.on(`connection`, (socket) => {
-  logger.info(`connected... id : ${socket.id}`);
-});
-
-setInterval(() => {
-  counter++;
-  mIo.emit(`counter`, { counter });
-}, 1000);
-
-module.exports = server;
+module.exports = socketLoader;
