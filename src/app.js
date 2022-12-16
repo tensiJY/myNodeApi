df91ccc30b29bc321d41992908b33bc06f67e259
tensiJY <32132377+tensiJY@users.noreply.github.com>
2022-12-16 14:49:51 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,64 @@
-const { port, nodeEnv } = require(`./config/keys`);
+const { port, nodeEnv, os } = require(`./config/keys`);
+const cluster = require(`cluster`);
 const logger = require(`./modules/logger`);
-const server = require(`./loaders/socketLoader`);
 
-server.listen(port, () => {
-  logger.info(`${nodeEnv} server is listening >>> localhost:${port}`);
-});
+const http = require(`http`);
+
+const server = require(`./loaders/serverLoader`);
+
+const { setupMaster, setupWorker } = require(`@socket.io/sticky`);
+const { createAdapter, setupPrimary } = require(`@socket.io/cluster-adapter`);
+const { Server } = require(`socket.io`);
+
+//  클러스터 : 마스터
+if (cluster.isMaster) {
+  logger.debug(`Master : ${process.pid} is running`);
+
+  const httpServer = http.createServer();
+
+  setupMaster(httpServer, {
+    loadBalancingMethod: 'least-connection',
+  });
+
+  setupPrimary();
+
+  //  Node.js < 16.0.0
+  //cluster.setupMaster({
+  //  serialization: "advanced",
+  //});
+  // Node.js > 16.0.0
+  cluster.setupPrimary({
+    serialization: 'advanced',
+  });
+
+  httpServer.listen(port, () => {
+    logger.debug(`${nodeEnv} server is listening >>> localhost:${port}`);
+  });
+
+  const workerLength = +os.cpus || 4;
+
+  for (let i = 0; i < workerLength; i++) {
+    cluster.fork();
+  }
+
+  cluster.on(`exit`, (worker) => {
+    logger.debug(`Worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  logger.debug(`Worker ${process.pid} started`);
+
+  const httpServer = server(http);
+
+  const io = new Server(httpServer, {
+    cors: {
+      origin: '*',
+    },
+  });
+
+  io.adapter(createAdapter());
+
+  setupWorker(io);
+
+  require(`./loaders/socketLoader`)(io);
+}
