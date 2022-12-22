48de1e085e11a876f9aab31d22aad7e175449682
tensiJY <32132377+tensiJY@users.noreply.github.com>
2022-12-22 16:57:17 +0900

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,35 +1,19 @@
-const express = require(`express`);
-const { body } = require(`express-validator`);
-const router = express.Router();
+const User = require(`../models/user`);
 
-const User = require(`../../models/user`);
-const authController = require(`../../controllers/authController`);
+const { validationResult } = require(`express-validator`);
 
-//  사용자 등록 -> put
-router.put(
-  `/signup`,
-  [
-    body(`email`)
-      .isEmail()
-      .withMessage(`please enter a vaild email.`)
-      .custom((value, { req }) => {
-        //  custom 발리데이션
-        //  만약 인증에 성공한다면 함수가 true를 반환하고
-        //  인증이 비동기식 작업을 한 경우네는 프로미스를 반환한다.
+exports.singup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    //  에러처리
+    //  new Error() 새로운 error 객체를 생성
+    const error = new Error(`Validaton failed`);
+    error.statusCode = 422;
+    //  자동으로 함수 실행을 종료하고
+    //  다음 오류 처리 함수나 미들웨어로 향함
+    error.data = errors.array();
+    throw error;
+  }
 
-        //  email의 값과 일치한 것을 찾음
-        return User.findOne({ email: value }).then((userDoc) => {
-          //  then에 입력하는 userDoc은 데이터베이스에서 찾은 사용자 객체로
-          if (userDoc) {
-            return Promise.reject("E-mail address already exists!");
-          }
-        });
-      })
-      .normalizeEmail(),
-    body("password").trim().isLength({ min: 5 }),
-    body("name").not().isEmpty(),
-  ],
-  authController.singup
-);
-
-module.exports = router;
+  const { email, name, password } = req.body;
+};
