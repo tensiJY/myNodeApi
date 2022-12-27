4447e61e6b45c0f6c933570e60b2ca795147d9cb
tensiJY <32132377+tensiJY@users.noreply.github.com>
2022-12-27 17:28:14 +0900

    Update socketLoader.js

--- src/loaders/socketLoader.js
+++ src/loaders/socketLoader.js
@@ -1,8 +1,25 @@
-const { drvSocket, mgrSocket } = require(`../modules/socket`);
+const { Server } = require(`socket.io`);
+const socket = require(`../modules/socket`);
 
-const socketLoader = (io) => {
-  drvSocket(io);
-  mgrSocket(io);
+let io, drv, mgr;
+
+module.exports = {
+  init: (httpServer) => {
+    io = new Server(httpServer, {
+      cors: {
+        origin: '*',
+      },
+    });
+
+    drv = socket.drvSocket(io);
+    mgr = socket.mgrSocket(io);
+
+    return io;
+  },
+  getIo: () => {
+    if (!io) {
+      throw new Error(`Socket.io not init`);
+    }
+    return io;
+  },
 };
-
-module.exports = socketLoader;
