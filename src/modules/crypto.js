f6dee096a2b6912d260e408da6428b1a8a997d71
tensiJY <32132377+tensiJY@users.noreply.github.com>
2022-12-29 16:15:50 +0900

    Update crypto.js

--- src/modules/crypto.js
+++ src/modules/crypto.js
@@ -1,5 +1,9 @@
 const bcrypt = require(`bcryptjs`); //  bcrypt
 
+const { AES_KEY } = require(`../config/keys`);
+
+var CryptoJS = require('crypto-js');
+
 /**
  * 비밀번호 암호화
  * @param {*} password
@@ -17,4 +21,45 @@ exports.hashSync = (password) => {
  */
 exports.compareSync = (password, encodedPassword) => {
   return bcrypt.compareSync(password, encodedPassword);
+};
+
+//  AES, CBC(default), PKCS7(default)
+/**
+ * 암호화
+ * @param {*} data
+ * @param {*} salt
+ * @returns
+ */
+exports.aesEncrypt = (data, salt = AES_KEY) => {
+  return CryptoJS.AES.encrypt(JSON.stringify(data), salt).toString();
+};
+
+/**
+ * 복호화
+ * @param {*} data
+ * @param {*} salt
+ * @returns
+ */
+exports.aesDecrypt = (data, salt = AES_KEY) => {
+  const bytes = CryptoJS.AES.decrypt(data, salt);
+  const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  return decryptedData;
+};
+
+/**
+ * 인코딩
+ * @param {*} data
+ * @returns
+ */
+exports.encode = (data) => {
+  return encodeURIComponent(data);
+};
+
+/**
+ * 디코딩
+ * @param {*} data
+ * @returns
+ */
+exports.decode = (data) => {
+  return decodeURIComponent(data);
 };
