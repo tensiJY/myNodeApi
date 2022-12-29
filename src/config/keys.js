152ce38f56a8bba7f65197f8e4f60e50798c00c3
tensiJY <32132377+tensiJY@users.noreply.github.com>
2022-12-29 16:13:29 +0900

    Update keys.js

--- src/config/keys.js
+++ src/config/keys.js
@@ -13,36 +13,24 @@ const ROBOT_TXT = path.join(__dirname, `/robots.txt`);
 const NODE_ENV = process.env.NODE_ENV || `development`;
 const isDev = NODE_ENV === `development`;
 
-//  포트 설정
-const PORT = +process.env.PORT || 80;
-
 //  데이터베이스
 const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 
-//  logger
-const LOG_DIR = process.env.LOG_DIR;
-
 //const serverCpus = os.cpus().length;
 //const envCups = +process.env.OS_CPUS;
 //const OS_CPUS = envCups > serverCpus ? 1 : envCups;
-const OS_CPUS = +process.env.OS_CPUS || 2;
 
 module.exports = {
-  nodeEnv: NODE_ENV,
+  NODE_ENV: NODE_ENV,
   isDev: isDev,
-  port: PORT,
-  db: {
-    host: DB_HOST,
-    user: DB_USER,
-    pass: DB_PASS,
-  },
-  log: {
-    dir: LOG_DIR,
-  },
-  os: {
-    cpus: OS_CPUS,
-  },
-  robotTxt: ROBOT_TXT,
+  PORT: +process.env.PORT || 80,
+  DB_HOST: DB_HOST,
+  DB_USER: DB_USER,
+  DB_PASS: DB_PASS,
+  LOG_DIR: process.env.LOG_DIR,
+  OS_CPUS: +process.env.OS_CPUS || 2,
+  ROBOT_TXT: ROBOT_TXT,
+  AES_KEY: process.env.AES_KEY,
 };
