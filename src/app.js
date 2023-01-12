a5cca0fb684116fe227feb37f60eaacb540f2896
tensiJY <32132377+tensiJY@users.noreply.github.com>
2023-01-12 17:07:05 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,6 +9,54 @@ const server = require(`./loaders/serverLoader`);
 const { setupMaster, setupWorker } = require(`@socket.io/sticky`);
 const { createAdapter, setupPrimary } = require(`@socket.io/cluster-adapter`);
 
+/*
+//  클러스터 : 마스터
+if (cluster.isMaster) {
+  logger.debug(`Master : ${process.pid} is running`);
+
+  const httpServer = http.createServer();
+
+  setupMaster(httpServer, {
+    loadBalancingMethod: 'least-connection',
+  });
+
+  setupPrimary();
+
+  //  Node.js < 16.0.0
+  //cluster.setupMaster({
+  //  serialization: "advanced",
+  //});
+  // Node.js > 16.0.0
+  cluster.setupPrimary({
+    serialization: 'advanced',
+  });
+
+  httpServer.listen(PORT, () => {
+    logger.debug(`${NODE_ENV} server is listening >>> localhost:${PORT}`);
+  });
+
+  const workerLength = OS_CPUS;
+
+  for (let i = 0; i < workerLength; i++) {
+    cluster.fork();
+  }
+
+  cluster.on(`exit`, (worker) => {
+    logger.debug(`Worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  logger.debug(`Worker ${process.pid} started`);
+
+  const httpServer = server(http);
+  const io = require(`./loaders/socketLoader`).init(httpServer);
+
+  io.adapter(createAdapter());
+
+  setupWorker(io);
+}
+*/
+
 const mongoose = require(`mongoose`);
 
 mongoose.set('strictQuery', false);
@@ -61,4 +109,8 @@ mongoose
       setupWorker(io);
     }
   })
-  .catch((err) => logger.error(err));
+  .catch((err) => {
+    logger.info(`server is shutdown..`);
+    logger.error(`mongo connection : error`);
+    logger.error(err);
+  });
