a9080a9e3d770fd73ea468be62668d5eedf4078b
tensiJY <32132377+tensiJY@users.noreply.github.com>
2023-01-12 17:06:30 +0900

    Update drvSocket.js

--- html/drvSocket.js
+++ html/drvSocket.js
@@ -1,11 +1,11 @@
 const logger = require(`../logger`);
 
-const Log = require('../../models/sample-log');
+const Log = require(`../../models/sample-log`);
+
+const namespace = `drv`;
 
 exports.drvSocket = (io) => {
-  let counter = 0;
-
-  const of = io.of(`/drv`);
+  const of = io.of(`/${namespace}`);
   of.use((socket, next) => {
     const header = socket.handshake.headers[`authorization`];
     logger.info(header);
@@ -13,23 +13,29 @@ exports.drvSocket = (io) => {
   });
 
   of.on(`connection`, (socket) => {
-    counter++;
     logger.debug(`drv connected... pid : ${process.pid},  id : ${socket.id}`);
 
-    socket.on(`setMsg`, (data) => {
-      logger.debug(`drv setMsg call`);
-      const jsonData = JSON.parse(data);
+    socket.on(`setMsg`, async (data) => {
+      //logger.debug(`drv setMsg call`);
+      //logger.debug(`drv setMsg data : ${data}`);
 
+      logger.info(`drv setMsg call`);
+      logger.info(`drv setMsg data : ${data}`);
+
+      const obj = JSON.parse(data);
       const log = new Log({
-        name: `${socket.id}`,
-        message: `${jsonData.msg}`,
+        socket_id: `${socket.id}`,
+        message: obj.msg,
+        type: `${namespace}`,
       });
 
-      log.save();
+      const savedMsg = await log.save();
+      logger.info(savedMsg);
+      const totalCount = await Log.find().countDocuments();
+      logger.info(`drv >>> totalCount : ${totalCount}`);
 
-      logger.debug(`drv setMsg data : ${data}`);
-      //const obj = JSON.parse(data);
       of.emit(`getMsg`, data);
+      logger.info('call getMsg client');
     });
   });
 
