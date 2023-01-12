704f2a0da0b84e1ec3a7843a73cd82e16c749af8
tensiJY <32132377+tensiJY@users.noreply.github.com>
2023-01-12 17:04:54 +0900

    Update keys.js

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,22 +1,22 @@
 const path = require(`path`);
 const dotenv = require(`dotenv`);
 
-//const os = require(`os`);
+//  개발모드인경우 : 개발 true, 운영 : false
+//  EXE build 이므로 환경변수 설정하지 않았을 경우 자동적으로 운영환경 설정
+
+const NODE_ENV = process.env.NODE_ENV || `production`;
 
 dotenv.config({
-  path: path.join(__dirname, `/.env.${process.env.NODE_ENV}`),
+  path: path.join(__dirname, `/.env.${NODE_ENV}`),
 });
 
 const ROBOT_TXT = path.join(__dirname, `/robots.txt`);
 
-//  개발모드인경우 : 개발 true, 운영 : false
-const NODE_ENV = process.env.NODE_ENV || `development`;
 const isDev = NODE_ENV === `development`;
 
-//  데이터베이스
-const DB_HOST = process.env.DB_HOST;
-const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.DB_PASS;
+const LOG_DIR = process.env.LOG_DIR;
+
+const LOG_DIR_PATH = process.pkg ? `.${LOG_DIR}` : LOG_DIR;
 
 //  몽고
 const MONGO_URL = process.env.MONGO_URL;
@@ -29,10 +29,7 @@ module.exports = {
   NODE_ENV: NODE_ENV,
   isDev: isDev,
   PORT: +process.env.PORT || 8080,
-  DB_HOST: DB_HOST,
-  DB_USER: DB_USER,
-  DB_PASS: DB_PASS,
-  LOG_DIR: process.env.LOG_DIR,
+  LOG_DIR: LOG_DIR_PATH,
   OS_CPUS: +process.env.OS_CPUS || 2,
   ROBOT_TXT: ROBOT_TXT,
   AES_KEY: process.env.AES_KEY,
